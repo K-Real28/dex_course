039aaa641e62eb71a99e6a59faf8fcbcceb89d5b
Gordienko Kirill <55459077+K-Real28@users.noreply.github.com>
2024-02-21 08:42:48 +0200

    Update task#2.dart

--- home_work#2/task#2.dart
+++ home_work#2/task#2.dart
@@ -1,1 +1,29 @@
+class DocumentFormatter {
+  
+  String createPdfDocument(String content) {
+    //логика
+    return 'PDF документ: $content';
+  }
+
+  String createHtmlDocument(String content) {
+    return 'HTML документ: $content';
+  }
+
+  String createDocsDocument(String content) {
+    return 'DOCS документ: $content';
+  }
+}
+
+void main() {
+  final formatter = DocumentFormatter();
+
+  final pdfContent = formatter.createPdfDocument('Содержимое PDF документа...');
+  print(pdfContent);
+
+  final htmlContent = formatter.createHtmlDocument('Содержимое HTML документа...');
+  print(htmlContent);
+
+  final docsContent = formatter.createDocsDocument('Содержимое DOCS документа...');
+  print(docsContent);
+}
 
