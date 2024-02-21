94f6d8f5d8c12b495fb739874835d68fea5b03b5
Gordienko Kirill <55459077+K-Real28@users.noreply.github.com>
2024-02-21 14:12:37 +0200

    Update task#4.dart

--- home_work#2/task#4.dart
+++ home_work#2/task#4.dart
@@ -1,1 +1,31 @@
+class AuthenticationManager {
+
+  Future<bool> login(String username, String password) async {
+     if (username == 'user' && password == 'password123') {
+       return true;
+     } else {
+       return false;
+     }
+
+  }
+
+  Future<void> logout() async {
+    // Здесь можно реализовать логику выхода пользователя
+  }
+
+  Future<bool> isAuthenticated() async {
+    // Здесь можно проверить, есть ли сохраненные сессии
+    return false;
+  }
+}
+
+void main() async {
+  final authManager = AuthenticationManager();
+  final loggedIn = await authManager.login('user', 'password123');
+  if (loggedIn) {
+    print('Пользователь успешно авторизован!');
+  } else {
+    print('Ошибка аутентификации. Проверьте учетные данные.');
+  }
+}
 
